7f67867aa6b9a53a9673d9c68ceac4d3d7880848
xiaoli <1480809371@qq.com>
2023-07-28 14:52:54 +0800

    fix:

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,6 +4,7 @@
     {{ head }}
     <div>测试测试测试branch-commit-checkout-commit</div>
     <div>feature/dev分支</div>
+    <div>feature/dev分支1111</div>
     <button @click="changeValue">更改父组件的值</button>
   </div>
 </template>
