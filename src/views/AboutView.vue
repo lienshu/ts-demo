b3555c3584dba2b1eab14b11a2997b4be621863e
xiaoli <1480809371@qq.com>
2023-08-17 17:50:05 +0800

    fix: ref属性value

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,15 +4,48 @@
     <div v-for="(_, tab) in tabs" :key="tab" @click="currentTab = tab">
       <component :is="tabs[currentTab]">{{ tab }}</component>
     </div>
-    
+    <a-form
+    :model="formState"
+    name="basic"
+    :label-col="{ span: 24 }"
+    :wrapper-col="{ span: 4 }"
+    autocomplete="off"
+  >
+    <a-form-item
+      label="Username"
+      name="username"
+      :rules="[{ required: true, message: 'Please input your username!' }]"
+    >
+      <a-input v-model:value="formState.username" @pressEnter="pressEnter" />
+    </a-form-item>
+    <a-form-item
+      label="en_username"
+      name="en_username"
+      :rules="[{ required: true, message: 'Please input your en_username!' }]"
+    >
+      <a-input v-model:value="formState.en_username" />
+    </a-form-item>
+  </a-form>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
-import AboutViewHead from './About/AboutViewHead.vue'
-import AboutViewWrap from './About/AboutViewWrap.vue'
+import { ref, reactive } from 'vue';
+import { pinyin } from 'pinyin-pro';
+import { capitalize, toString, join } from 'lodash';
+import AboutViewHead from './About/AboutViewHead.vue';
+import AboutViewWrap from './About/AboutViewWrap.vue';
 import AboutViewFooter from './About/AboutViewFooter.vue'
+
+interface FormState {
+  username: string;
+  en_username: string;
+}
+
+const formState = ref<FormState>({
+  username: '',
+  en_username: '',
+});
 
 const tabs = {
   AboutViewHead,
@@ -22,8 +55,29 @@ const tabs = {
 
 const currentTab = ref('AboutViewHead')
 
-const changeComponent = () => {
+const convertToPinyin = (text = "", mode = "normal") => {
+  const arr = pinyin(text, {
+    mode: 'normal',
+    toneType: "none",
+    removeNonZh: true,
+    nonZh: "removed",
+    type: 'array'
+  })
+  if (!arr) return ''
+  return capitalize(join(arr, ''));
+}
 
+const a = ref<{name: string, address: string}>({name: '张三', address: '北京'})
+
+const pressEnter = () => {
+  const {username} = formState.value
+  formState.value = { ...formState.value, en_username: convertToPinyin(username) }
+  // console.log({...})
+  console.log(formState.value, 'formState.value')
+  console.log(formState.value.username, formState.value.en_username)
+
+  a.value = {...a.value, name: '李四' }
+  console.log(a, 'a')
 }
 </script>
 
