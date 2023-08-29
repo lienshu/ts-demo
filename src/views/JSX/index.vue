67c37ece10d4a35b9e4af8ba30b24da94dec0a31
xiaoli <1480809371@qq.com>
2023-08-29 17:04:04 +0800

    fix: vue-cli&jsx

---
+++ src/views/JSX/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { defineComponent, ref } from "vue"
+
+export default defineComponent({
+  setup() {
+    const message = ref('vue-cli结合jsx写法')
+    return () => {
+      <div>
+        {message.value}
+      </div>
+    }
+  }
+})
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
